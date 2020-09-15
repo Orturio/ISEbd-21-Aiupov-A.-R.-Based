c125f3fcc2d086a5b132130a206f89a504a7ee51
Orturio <60659438+Orturio@users.noreply.github.com>
2020-09-15 16:30:54 +0400

    fixed names

--- DrawAirplan/DrawAirplan/FormAirplan.cs
+++ DrawAirplan/DrawAirplan/FormAirplan.cs
@@ -35,7 +35,7 @@ namespace DrawAirplan
         private void buttonCreate_Click(object sender, EventArgs e)
         {
             Random rnd = new Random();
-            airplan = new Airbus(rnd.Next(250, 450), rnd.Next(1000, 2000), Color.White,
+            airplan = new Airbus(rnd.Next(250, 450), rnd.Next(1000, 2000), Color.LightBlue,
             Color.Blue, true, true, true);
             airplan.SetPosition(rnd.Next(10, 100), rnd.Next(10, 100), pictureBoxAirplans.Width,
             pictureBoxAirplans.Height);
