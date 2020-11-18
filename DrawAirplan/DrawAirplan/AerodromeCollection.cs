65855bcbf083ca3f498a8dd5d23c7ba018e590a0
gusevsvoloch@gmail.com <gusevsvoloch@gmail.com>
2020-11-18 19:41:13 +0400

    faf

--- DrawAirplan/DrawAirplan/AerodromeCollection.cs
+++ DrawAirplan/DrawAirplan/AerodromeCollection.cs
@@ -1,8 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace DrawAirplan
 {
@@ -15,7 +14,9 @@ namespace DrawAirplan
         private readonly int pictureWidth;
       
         private readonly int pictureHeight;
-        
+
+        private readonly char separator = ':';
+
         public AerodromeCollection(int pictureWidth, int pictureHeight)
         {
             aerodromeStages = new Dictionary<string, Aerodrome<Vehicle>>();
@@ -54,5 +55,100 @@ namespace DrawAirplan
                 }
             }
         }
+
+        public bool SaveData(string filename)
+        {
+            if (File.Exists(filename))
+            {
+                File.Delete(filename);
+            }
+            using (FileStream fs = new FileStream(filename, FileMode.Create))
+            {
+                using (StreamWriter writer = new StreamWriter(fs))
+                {
+                    writer.Write($"AerodromeCollection{Environment.NewLine}", fs);
+                    foreach (var level in aerodromeStages)
+                    {
+                        //Начинаем парковку
+                        writer.Write($"Aerodrome{separator}{level.Key}{Environment.NewLine}", fs);
+                        ITransport aircraft = null;
+                        for (int i = 0; (aircraft = level.Value.GetNext(i)) != null; i++)
+                        {
+                            if (aircraft != null)
+                            {
+                                //если место не пустое
+                                //Записываем тип машины
+                                if (aircraft.GetType().Name == "Aircraft")
+                                {
+                                    writer.Write($"Aircraft{separator}", fs);
+                                }
+                                if (aircraft.GetType().Name == "Airbus")
+                                {
+                                    writer.Write($"Airbus{separator}", fs);
+                                }
+                                //Записываемые параметры
+                                writer.Write(aircraft + Environment.NewLine, fs);
+                            }
+                        }
+                    }
+                }                
+            }
+            return true;
+        }
+
+        public bool LoadData(string filename)
+        {
+            if (!File.Exists(filename))
+            {
+                return false;
+            }
+
+            using (StreamReader sr = new StreamReader(filename))
+            {
+                string line = sr.ReadLine();
+                if (line.Contains("AerodromeCollection"))
+                {
+                    //очищаем записи
+                    aerodromeStages.Clear();
+                }
+
+                else
+                {
+                    //если нет такой записи, то это не те данные
+                    return false;
+                }
+
+                line = sr.ReadLine();
+                Vehicle aircraft = null;
+                string key = string.Empty;
+                while (line != null && line.Contains("Aerodrome"))
+                {
+                    key = line.Split(separator)[1];
+                    aerodromeStages.Add(key, new Aerodrome<Vehicle>(pictureWidth, pictureHeight));
+                    line = sr.ReadLine();
+                    while (line != null && (line.Contains("Aircraft") || line.Contains("Airbus")))
+                    {
+                        if (line.Split(separator)[0] == "Aircraft")
+                        {
+                            aircraft = new Aircraft(line.Split(separator)[1]);
+                        }
+
+                        else if (line.Split(separator)[0] == "Airbus")
+                        {
+                            aircraft = new Airbus(line.Split(separator)[1]);
+                        }
+
+                        var result = aerodromeStages[key] + aircraft;
+                        if (!result)
+                        {
+                            return false;
+                        }
+
+                        line = sr.ReadLine();
+                    }
+                }
+            }
+            return true;
+        }        
     }
 }
